5443e99fb60b6b4d01d5e5d5736289d7857c54c9
Marcin <mprzytula94@gmail.com>
2022-04-03 17:35:11 +0200

    added cors adress

--- index.js
+++ index.js
@@ -12,8 +12,22 @@ require('./db/mongoose');
 // Content-type: application/json
 app.use(bodyParser.json());
 
-//fix cord domeny rozne
-app.use(cors());
+//fix cors domeny rozne
+const whitelist = [
+  'https://marcinprzytula.github.io',
+  'http://localhost:3001',
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 
 //routes
 app.use('/api/', apiRouter);
