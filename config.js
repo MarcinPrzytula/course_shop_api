7de75464f49123fff7967d42b6c3ab4de44b12ef
Marcin <mprzytula94@gmail.com>
2022-04-03 17:03:28 +0200

    added db

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ module.exports = {
   port: process.env.PORT || 3001,
   database:
     process.env.DATABASE ||
-    'mongodb://127.0.0.1:27017/courseShop',
+    'mongodb+srv://admin:admin325@cluster0.0ywy7.mongodb.net/courseShop?retryWrites=true&w=majority',
+  // 'mongodb://127.0.0.1:27017/courseShop',
 };
