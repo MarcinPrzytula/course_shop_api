a22fe0289013591bb699c0d080b43d54d87b4a2e
Marcin <mprzytula94@gmail.com>
2022-05-05 19:24:25 +0200

    added app.set('trust proxy', 1)

--- index.js
+++ index.js
@@ -23,10 +23,10 @@ app.use(bodyParser.json()); // parser - Content-type: application/json
 const DB =
   'mongodb+srv://admin:admin325@cluster0.0ywy7.mongodb.net/courseShop?retryWrites=true&w=majority';
 
-// app.set('trust proxy', 1);
-app.use(cookieParser());
+app.set('trust proxy', 1);
+// app.use(cookieParser());
 
-// app.use(cookieParser('secretcode'));
+app.use(cookieParser('secretcode'));
 
 const cookie = {
   secure: true,
